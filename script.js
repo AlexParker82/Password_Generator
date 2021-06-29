dab1a5be6bae02894814399f3db93c4bc277d1f3
Alex Parker <alex.n.parker82@gmail.com>
2021-06-29 17:53:56 -0400

    linking html elements to JS using IDs

--- script.js
+++ script.js
@@ -5,3 +5,13 @@ output.innerHTML = slider.value;
 slider.oninput = function() {
   output.innerHTML = this.value;
 }
+
+const formDom = document.querySelector("#password-form");
+const displayDom = document.querySelector("#password-display");
+const copybtnDOM = document.querySelector("#copy");
+const  lengthDOM = document.querySelector("#how-long");
+const upperDOM = document.querySelector("#uppercase");
+const lowerDOM = document.querySelector("#lowercase");
+const numbersDOM = document.querySelector("#numbers");
+const symbolsDOM = document.querySelector("#symbols");
+const generatebtnDOM = document.querySelector("#generate");
